c66a4fed11cd08f21383b649b46046f2a100fdb0
emex-nord <128908090+emex-nord@users.noreply.github.com>
2023-07-08 12:06:54 +0300

    Domino_Piling.py

--- Domino_Piling.py
+++ Domino_Piling.py
@@ -1,6 +1,8 @@
-def dominopilling(M,N):
-    "M and N are the rows and/or columns of the square board"
-    assert 1 <= M <= N <= 16,' the value of M and N shoul conforrm this: 1<=M<=N<=16' 
+def domino_piling(M, N):
+    assert 1 <= M <= N <= 16, "The values of M and N should conform to the constraints: 1 <= M <= N <= 16"
     total_sqr=M*N
     output=total_sqr//2
     return output
+
+M, N = map(int, input().split())
+print(domino_piling(M, N))
